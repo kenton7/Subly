5775353b4f85fde1b67a5b015fa1a4af19a022c2
kenton7 <63309306+kenton7@users.noreply.github.com>
2021-02-11 00:03:30 +0300

    added animations

--- Subly/Controllers/AddViewController.swift
+++ Subly/Controllers/AddViewController.swift
@@ -37,6 +37,35 @@ class AddViewController: UIViewController {
         definesPresentationContext = true
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        animateTable()
+    }
+    
+    ///анимация table view
+    private func animateTable() {
+        tableView.reloadData()
+            
+        let cells = tableView.visibleCells
+        let tableHeight: CGFloat = tableView.bounds.size.height
+            
+        for i in cells {
+            let cell: UITableViewCell = i as UITableViewCell
+            cell.transform = CGAffineTransform(translationX: 0, y: tableHeight)
+        }
+            
+        var index = 0
+            
+        for a in cells {
+            let cell: UITableViewCell = a as UITableViewCell
+            UIView.animate(withDuration: 1.5, delay: 0.05 * Double(index), usingSpringWithDamping: 0.8, initialSpringVelocity: 0, animations: {
+                cell.transform = CGAffineTransform(translationX: 0, y: 0);
+            }, completion: nil)
+                
+            index += 1
+        }
+    }
+    
     private func setupContent() {
         subsArray.append(ImagesAndNames(imageName: "1Blocker", name: "1Blocker"))
         subsArray.append(ImagesAndNames(imageName: "onePassword", name: "1Password"))
@@ -140,8 +169,25 @@ class AddViewController: UIViewController {
       }
    }
     
+//    func dismissAlert() {
+//
+//        let bounds = view.bounds
+//        let smallFrame = view.frame.insetBy(dx: view.frame.size.width / 4, dy: view.frame.size.height / 4)
+//        let finalFrame = smallFrame.offsetBy(dx: 0, dy: bounds.size.height)
+//
+//
+//        UIView.animateKeyframes(withDuration: 4, delay: 0, options: .calculationModeCubic, animations: {
+//            UIView.addKeyframe(withRelativeStartTime: 0.0, relativeDuration: 0.5) {
+//                self.view.frame = smallFrame
+//            }
+//            UIView.addKeyframe(withRelativeStartTime: 0.5, relativeDuration: 0.5) {
+//                self.view.frame = finalFrame
+//            }
+//        }, completion: nil)
+//    }
+    
     @IBAction func unwindSegue(_ segue: UIStoryboardSegue) {
-        
+        //dismissAlert()
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
--- Subly/Controllers/MainCollectionView.swift
+++ Subly/Controllers/MainCollectionView.swift
@@ -44,6 +44,40 @@ class MainViewController: UIViewController {
         startListeningForConversations()
     }
     
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        animateTable()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        animateTable()
+    }
+    
+    ///анимация table view
+    private func animateTable() {
+        tableView.reloadData()
+            
+        let cells = tableView.visibleCells
+        let tableHeight: CGFloat = tableView.bounds.size.height
+            
+        for i in cells {
+            let cell: UITableViewCell = i as UITableViewCell
+            cell.transform = CGAffineTransform(translationX: 0, y: tableHeight)
+        }
+            
+        var index = 0
+            
+        for a in cells {
+            let cell: UITableViewCell = a as UITableViewCell
+            UIView.animate(withDuration: 1.5, delay: 0.05 * Double(index), usingSpringWithDamping: 0.8, initialSpringVelocity: 0, animations: {
+                cell.transform = CGAffineTransform(translationX: 0, y: 0);
+            }, completion: nil)
+                
+            index += 1
+        }
+    }
+    
     private func setupGradient() {
         view.layer.addSublayer(gradientLayer)
 //        if let startColor = startColor, let endColor = endColor {
